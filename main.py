ea390107e576339215447a0c1326ff2598095ffb
eugeneokaka <81880459+eugeneokaka@users.noreply.github.com>
2024-08-13 09:34:38 +0300

    remove mat second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 # st.write("hello ")
 # st.write({"key":["one","two"]})
 st.title("simple Data dashbord")
